020e8fb9055354fc722997cfd3b28a48daab235f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-27 23:32:26 +0530

    Hash Password with bcryptjs

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,23 @@
-const User = require("../models/User.model");
 const { StatusCodes } = require("http-status-codes");
+const bcryptjs = require("bcryptjs");
+
+const User = require("../models/User.model");
+const { BadRequestError } = require("../errors")
 
 const login = async (req, res) => {
   res.json({ msg: "Login User" })
 }
 
 const register = async (req, res) => {
-  const userToRegister = { ...req.body };
+  const { name, email, password } = req.body;
+
+  const salt = await bcryptjs.genSalt(10);
+  const passwordHash = await bcryptjs.hash(password, salt);
+  const userToRegister = {
+    name,
+    email,
+    password: passwordHash,
+  };
 
   const user = await User.create(userToRegister);
 
