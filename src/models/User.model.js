b6ee465a532bd52b2e6cdf927b840c2b711a9487
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-27 23:06:31 +0530

    Add User Model

---
+++ src/models/User.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: 3,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Please provide valid email"
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+  },
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+
+    delete returnedObj.password;
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
