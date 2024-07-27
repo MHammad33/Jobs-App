b1c6f06f1023ec0d377173d49832cd6fef3b6458
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-27 23:16:44 +0530

    Add Register User functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,16 @@
+const User = require("../models/User.model");
+const { StatusCodes } = require("http-status-codes");
+
 const login = async (req, res) => {
   res.json({ msg: "Login User" })
 }
 
 const register = async (req, res) => {
-  res.json({ msg: "Register User" })
+  const userToRegister = { ...req.body };
+
+  const user = await User.create(userToRegister);
+
+  res.status(StatusCodes.CREATED).json({ user });
 }
 
 module.exports = {
