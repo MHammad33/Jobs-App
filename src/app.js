612831f14649b670c31e809b2aec1fb90e6b486f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-28 21:35:11 +0500

    Add Security Packages

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 require("express-async-errors");
-const express = require("express");
+
+// Extra Security Packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+
 const authRouter = require("./routes/auth.routes");
 const jobRouter = require("./routes/jobs.routes");
 const middlewares = require("./middleware");
 
+
+const express = require("express");
 const app = express();
 
 // Middlewares
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // 100 requests per 15 minutes
+  }))
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 
 // Routes
 app.get("/", (req, res) => {
